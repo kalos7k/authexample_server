020b843a8591983f127b5c072fa93cf7af6cb692
Carlos Wang <carlos@schedulesavvy.com>
2014-02-08 19:26:54 -0800

    belated init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthexampleWebapp::Application.config.secret_key_base = '47a5cdab3d9c43b1f21ee1eba15bdc70214b01040ee7532afbd7b306b223262d3f6eceb3aa75fe07529db17334ae9ba82a35b49c45050c7b8a81b2a4db7dbf1e'
