330453010dd796fe523db14d51d988ffe43c6f92
Carlos Wang <carlos@schedulesavvy.com>
2014-02-10 17:32:25 -0800

    no token, sign in from curl works

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails.application.routes.default_url_options[:host] = '???'
+
 # Initialize the Rails application.
 AuthexampleWebapp::Application.initialize!
+
+
